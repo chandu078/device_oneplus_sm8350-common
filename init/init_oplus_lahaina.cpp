02c702bab10e225377a6459b225c2437dc90bb05
chandu078 <chandudyavanapelli03@gmail.com>
2022-06-08 04:57:51 +0000

    fixup! sm8350-common: init: Set model based on RF version
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- init/init_oplus_lahaina.cpp
+++ init/init_oplus_lahaina.cpp
@@ -16,15 +16,14 @@ using android::base::GetProperty;
  * does not work for our use case. Write "OverrideProperty" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
+void property_override(char const prop[], char const value[]) {
+  prop_info *pi;
 
-    prop_info* pi = (prop_info*)__system_property_find(name);
-    if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
-    }
+  pi = (prop_info *)__system_property_find(prop);
+  if (pi)
+    __system_property_update(pi, value, strlen(value));
+  else
+    __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 /*
@@ -33,46 +32,79 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
-    auto device = GetProperty("ro.product.product.device", "");
-    auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
-
-    switch (rf_version) {
-        case 11: // CN
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2110");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2120");
-            }
-            break;
-        case 12: // TMO
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2117");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2127");
-            }
-            break;
-        case 13: // IN
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2111");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2121");
-            }
-            break;
-        case 21: // EU
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2113");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2123");
-            }
-            break;
-        case 22: // NA
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2115");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2125");
-            }
-            break;
+  std::string project_codename = android::base::GetProperty("ro.boot.project_codename", "");
+  int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
+  if(project_codename == "lemonade"){
+      /* OnePlus 9 */
+      switch (rf_version){
+        case 11:
+          /* China */
+          property_override("ro.product.model", "LE2110");
+          break;
+        case 13:
+          /* India */
+          property_override("ro.product.model", "LE2111");
+          break;
+        case 21:
+          /* Europe */
+          property_override("ro.product.model", "LE2113");
+          break;
+        case 22:
+          /* Global / US Unlocked */
+          property_override("ro.product.model", "LE2115");
+          break;
         default:
-            LOG(ERROR) << "Unexpected RF version: " << rf_version;
-    }
+          /* Generic */
+          property_override("ro.product.model", "LE2115");
+          break;
+      }
+  } else if(project_codename == "lemonadet"){
+      /* OnePlus 9 T-Mobile */
+      switch (rf_version){
+        case 12:
+          /* T-Mobile */
+          property_override("ro.product.model", "LE2117");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2115");
+          break;
+      }
+  } else if(project_codename == "lemonadep"){
+      /* OnePlus 9 Pro */
+      switch (rf_version){
+        case 11:
+          /* China */
+          property_override("ro.product.model", "LE2120");
+          break;
+        case 13:
+          /* India */
+          property_override("ro.product.model", "LE2121");
+          break;
+        case 21:
+          /* Europe */
+          property_override("ro.product.model", "LE2123");
+          break;
+        case 22:
+          /* Global / US Unlocked */
+          property_override("ro.product.model", "LE2125");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2125");
+          break;
+      }
+  } else if(project_codename == "lemonadept"){
+      /* OnePlus 9 Pro T-Mobile */
+      switch (rf_version){
+        case 12:
+          /* T-Mobile */
+          property_override("ro.product.model", "LE2127");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2125");
+          break;
+      }
+   }
 }
